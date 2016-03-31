ae8685839d07d60caa6b68675821a48bf8094f8e
Murhaf Sousli <bboymarkor@gmail.com>
2016-03-31 06:52:29 +0300

    major updates

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta charset="<?php bloginfo('charset'); ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title(); ?></title>
-    <link rel="profile" href="http://gmpg.org/xfn/11" />
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <link rel="profile" href="http://gmpg.org/xfn/11"/>
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
     <base href="/murhaf/">
     <?php wp_head(); ?>
 </head>
 <body>
-    <app>
-        Loading...
-    </app>
-    <?php wp_footer(); ?>
+<app>
+    Loading...
+</app>
+<?php wp_footer(); ?>
 </body>
 </html>
